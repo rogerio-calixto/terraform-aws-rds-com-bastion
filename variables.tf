e4e64955112ad116d7897927d2c562586368e2e1
Rogerio Calixto <107159671+rogerio-calixto@users.noreply.github.com>
2023-05-31 12:45:20 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,28 @@
 variable "region" {
   default = "us-east-1"
 }
-variable "keypair-name" {
-  default = "devops-keypair"
+variable "vpc-id" {
+  default = "vpc-038bf2e0721bf2794"
 }
-
-variable "vpc-id" {}
-variable "private-avaiable_zone" {}
+variable "private-avaiable_zone" {
+  default = "us-east-1a"
+}
 variable "private-subnet-ids" {
   type    = list(string)
+  default =  [ "subnet-06f3baf568aeb1973", "subnet-0eda588007e5ab73f" ]
 }
-variable "sg-bastion-host-id" {}
-variable "db_identifier" {}
-variable "db_name" {}
-variable "username" {}
-variable "password" {}
+variable "sg-bastion-host-id" {
+  default = "sg-0da554cfb962c37fe"
+}
+variable "db_identifier" {
+  default = "devops-rds"
+}
+variable "db_name" {
+  default = "devops_db"
+}
+variable "username" {
+  default = "devops"
+}
+variable "password" {
+  default = "devops123"
+}
